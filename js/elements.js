db5ffbea77c28e793b51ae5926839043005cf299
Nick Rabone <n.rabone72@aol.com>
2024-07-20 04:17:12 +0100

    Implemented the grid system on the playground area

--- js/elements.js
+++ js/elements.js
@@ -1,11 +1,18 @@
 class Elements {
   constructor(cardsNumber) {
     this.cardsNumber = cardsNumber
-    this.plground = document.querySelector('.playground')
+    this.playground = document.querySelector('.playground')
     this.createCard()
   }
   createCard() {
-    console.log(this.cardsNumber)
+    this.playground.style.gridTemplateRows =
+      `
+      repeat(${Math.sqrt(this.cardsNumber)}, 1fr)
+      `
+    this.playground.style.gridTemplateColumns =
+      `
+      repeat(${Math.sqrt(this.cardsNumber)}, 1fr)
+      `
   }
 }
 
