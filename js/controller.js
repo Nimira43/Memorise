89f5528f6e6f39d1f52459080fa24c112155c8a3
Nick Rabone <n.rabone72@aol.com>
2024-07-20 20:21:34 +0100

    Developing game logic

--- js/controller.js
+++ js/controller.js
@@ -3,6 +3,7 @@ import Elements from './elements.js'
 class Controller {
   constructor(cardsNumber) {
     this.elements = new Elements(cardsNumber)
+    this.prevCard
     this.clickCard()
   }
 
@@ -12,6 +13,21 @@ class Controller {
       card.addEventListener('click', () => {
         attempts.click++
         card.classList.add('change')
+        if (EventSource.click === 2) {
+          cards.forEach((card) => {
+            card.classList.add('pause')
+            setTimeout(() => {
+              card.classList.remove('pause')
+            }, 1000)
+          })
+          if (this.prevCard.dataset.index === card.dataset.index) {
+            attempts.correct++
+            card.classList.add('stop')
+            this.prevCard.classList.add('stop')
+          }
+        } else {
+          this.prevCard = card
+        }
       })
     })
   }
