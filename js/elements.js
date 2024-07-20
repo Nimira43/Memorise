6764d44d31be07ff62e23b1462e9235177b751ed
Nick Rabone <n.rabone72@aol.com>
2024-07-20 04:30:43 +0100

    Setting up card indices for all grids and push this to a double index.

--- js/elements.js
+++ js/elements.js
@@ -2,8 +2,19 @@ class Elements {
   constructor(cardsNumber) {
     this.cardsNumber = cardsNumber
     this.playground = document.querySelector('.playground')
+    this.cardsIndex = []
+    this.doubleIndex()
     this.createCard()
   }
+  doubleIndex() {
+    for (let i = 1; i <= this.cardsNumber; i++) {
+      i <= this.cardsNumber / 2
+        ? this.cardsIndex.push(i)
+        : this.cardsIndex.push(i - this.cardsNumber / 2)
+    }
+    console.log(this.cardsIndex)
+  }
+
   createCard() {
     this.playground.style.gridTemplateRows =
       `
